720f349d295d5788978f5d40cfbf97e41545fe24
royzhuo <731924600@qq.com>
2017-09-28 14:15:52 +0800

    lucene test与github整合

---
+++ src/main/java/com/main/HelloWrold.java
@@ -0,0 +1,9 @@
+package com.main;/**
+ * Created by roy.zhuo on 2017/9/28.
+ */
+
+public class HelloWrold {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
